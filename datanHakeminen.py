68f997b000f32b68f709f48c25db9472e438aacf
Sonja <sonja.oehlandt@brightstraining.com>
2022-10-25 15:05:17 +0300

    datan hakeminen json muodossa netistä

--- datanHakeminen.py
+++ datanHakeminen.py
@@ -2,6 +2,9 @@ from urllib.request import urlopen
 import json
 url = "https://catfact.ninja/fact"
 response = urlopen(url)
-data_json = json.loads(response.read())
+data_json =json.loads(response.read())
 
-print(data_json)
+my_list = []
+for data in range(5):
+    my_list.append(data_json)
+print(my_list)
