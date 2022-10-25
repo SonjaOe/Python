650bf8edd81fa70fc280cc948c529cef6479b3b0
Sonja <sonja.oehlandt@brightstraining.com>
2022-10-25 15:10:55 +0300

    eri datat haetuissa datoissa

--- datanHakeminen.py
+++ datanHakeminen.py
@@ -1,10 +1,15 @@
 from urllib.request import urlopen
 import json
-url = "https://catfact.ninja/fact"
-response = urlopen(url)
-data_json =json.loads(response.read())
+#url = "https://catfact.ninja/fact"
+#response = urlopen(url)
+#data_json =json.loads(response.read())
 
 my_list = []
 for data in range(5):
+    url = "https://catfact.ninja/fact"
+    response = urlopen(url)
+    data_json =json.loads(response.read())
     my_list.append(data_json)
-print(my_list)
+
+for item in my_list:
+    print(item)
